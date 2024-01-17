b6d6a71e15ee9d2640c05763d6049acc23726269
Onyackenalai <156873509+Onyackenalai@users.noreply.github.com>
2024-01-17 07:36:07 +0300

    Add files via upload

---
+++ lab10.pas
@@ -0,0 +1,18 @@
+﻿uses lab10_unit;
+var i, j: integer;
+matrixA: matrix;
+matrixB: matrix;
+begin {главная программа}
+  for i:=1 to 4 do
+    for j:=1 to 4 do begin
+      matrixA[i, j] := random(-8, 8);
+    end;
+    
+   for i:=1 to 17 do
+    for j:=1 to 9 do begin
+      matrixB[i, j] := random(-8, 8);
+    end
+    chet(matrixA);
+    writeln('--------------------');
+    chet(matrixB)
+end.
